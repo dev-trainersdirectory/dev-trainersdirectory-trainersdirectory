599f43d46f5f3ce71975955b5f71ee21350969db
navalevishal <navale.vishal3@gmail.com>
2016-06-22 18:30:22 +0530

    config

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://demo.trainersdirectory.in/';
+$config['base_url'] = 'http://td.lcl/';
 
 /*
 |--------------------------------------------------------------------------
