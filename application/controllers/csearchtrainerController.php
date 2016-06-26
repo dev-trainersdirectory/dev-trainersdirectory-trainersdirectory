df7c9cc8ba80d039ba809499a6615d16a543dfbe
navalevishal <navale.vishal3@gmail.com>
2016-06-26 14:47:12 +0530

    Changes

--- application/controllers/csearchtrainerController.php
+++ application/controllers/csearchtrainerController.php
@@ -4,18 +4,5 @@ require_once( getcwd(). '/application/controllers/CSystemController.php' );
 
 class CSearchTrainerController extends CSystemController {
 
-	public function index() {
-		$this->search();
-	}
-
-	public function search() {
-		//$intSubjectId = $this->input->post( 'search_subject_id' );
-
-		$arrObjTrainers = CTrainers::fetchAllActiveTrainersBySubjectIdByCityId( 1, 1, $this->db );
-
-		print "<pre>";
-print_r($arrObjTrainers);
-print "</pre>";
-die;
 	}
 }
