d900b7721326e7d65aeeff1d483585f932527c69
Aniket Deshbhratar <aniketdeshbhratar.09@gmail.com>
2016-07-18 01:15:51 +0530

    Added search result page -top search bar -profile slider -mini details view -footer

--- templates/js/script.js
+++ templates/js/script.js
@@ -1,17 +1,35 @@
 // Carousel Auto-Cycle
   $(document).ready(function() {
+
+
+    //for flipper
+    $('.trainer-introduction').click(function (e) {
+        e.preventDefault();
+        $(this).parents("div.flipper").find('.front').toggleClass('hide show');
+        $(this).parents("div.flipper").find('.back').toggleClass('show hide');
+    });
+
+
     // for carousel
   $(".carousel").carousel({interval:false});
     $('.modal').on('shown.bs.modal', function (e) {
     $('.carousel').carousel('pause');
   })
 
+
+   // for filter dropdown
+    $(".select-filter li a").click(function(){
+    $(this).parents(".dropdown").find('.btn').html($(this).text() + '<i class="fa fa-chevron-down"></i>');
+    $(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+});
+
+
+
+
+
+
 });
 // for hide show div
 $(function () {
-    $('.more-details').click(function (event) {
-        event.preventDefault();
-        $(this).parents("div.flipper").find('.front').toggleClass('hide show');
-        $(this).parents("div.flipper").find('.back').toggleClass('show hide');
-    });
+
 });
